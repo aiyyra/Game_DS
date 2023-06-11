8dee941992c96aeb48cc163ae92e538589dcf40c
ayyra <muhdafiq197@gmail.com>
2023-06-11 11:11:41 +0800

    add leaderboard end class for tokeykusayang

---
+++ src/main/java/Scenes/LeaderboardEnd.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Scenes;
+
+import java.awt.Graphics;
+import main.game;
+
+/**
+ *
+ * @author HP
+ */
+public class LeaderboardEnd extends GameScenes implements SceneMethod{
+
+    public LeaderboardEnd(game game) {
+        super(game);
+    }
+    @Override
+    public void render(Graphics g) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseClicked(int x, int y) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseMoved(int x, int y) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mousePressed(int x, int y) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseReleased(int x, int y) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    public static void main(String[] args) {
+        System.out.println("hai");
+    }
+}
